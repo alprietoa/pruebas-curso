12992fd2f62180d828d475b62b80543fdcca90a0
Alberto P <alberto.parranz@gmail.com>
2026-09-16 11:44:17 +0200

    Cambios en 02_Variables y 03_Control_de_flujo

--- 03_control_de_flujo/ejemplo_codificacion.py
+++ 03_control_de_flujo/ejemplo_codificacion.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-print "En el Ñágara encontré un Ñandú"
+print ("En el Ñágara encontré un Ñandú")
 
 # Produciendo la siguiente salida:
 # En el Ñágara encontré un Ñandú
--- 03_control_de_flujo/primos.py
+++ 03_control_de_flujo/primos.py
@@ -9,9 +9,9 @@ multiplos = []
 primo="si"
 for i in range(2,valor):
 	if valor % i == 0:
-	    print( valor, " no es un numero primo.")
-	    primo="no"
-	    break
+		print( valor, " no es un numero primo.")
+		primo="no"
+		break
 
 if primo == "si":
     print( valor, " es un numero primo")
